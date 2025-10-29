45e637c85389b1118ff3a2fe563c5b63c226e5f8
z.mivechi <mivechi.work@gmail.com>
2025-10-29 11:37:49 +0330

    task #add layout

---
+++ app/gallery/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import ToggleButton from "@/Components/ToggleThemeButton/ToggleButton";
+
+export default function PhotosLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="py-2 sm:px-10 xs:px-4">
+        <ToggleButton />
+      </div>
+      {modal}
+      {children}
+    </>
+  );
+}
